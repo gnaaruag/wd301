c5931949b853b6c88ad30f7e1fcf7cd4cd39ebda
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-04-30 20:21:20 +0530

    comments

--- smarter-tasks/src/pages/comments/CommentList.tsx
+++ smarter-tasks/src/pages/comments/CommentList.tsx
@@ -3,29 +3,25 @@ import { useCommentsState } from "../../context/comment/context";
 const CommentList = () => {
   const commentState = useCommentsState();
 
-  // Destructure necessary values from commentState
   const { comments, isLoading, isError, errorMessage } = commentState;
 
-  // Check if there's an error fetching comments
   if (isError) {
     return <div>Error: {errorMessage}</div>;
   }
 
-  // Check if comments are still loading
   if (isLoading) {
     return <div>Loading comments...</div>;
   }
 
   // Get the current user from localStorage
   const currentMember = JSON.parse(localStorage.getItem("userData") || "{}");
-  // const currentUser = currentMember.name.toString();
   const User = currentMember.name.toString();
   return (
     <div className="mt-4">
       <h3 className="text-lg font-medium">Comments</h3>
       <div className="mt-2">
         {comments.length === 0 ? (
-          <div>No comments available</div>
+          <div>No comments yet</div>
         ) : (
           comments.map((comment) => (
             <div key={comment.id} className="comment">
--- smarter-tasks/src/pages/comments/NewComment.tsx
+++ smarter-tasks/src/pages/comments/NewComment.tsx
@@ -33,12 +33,13 @@ const NewComment = () => {
         console.error("User data not found in local storage.");
         return;
       }
-      const currentMember = JSON.parse(currentMemberString);
+      const currentUser = JSON.parse(currentMemberString);
       console.log(formData.comment);
-      if (currentMember.name) {
+      if (currentUser.name) {
+		console.log('ueee')
         const comment = {
           description: formData.comment,
-          name: currentMember.name.toString(),
+          name: currentUser.name.toString(),
           timestamp: new Date().toISOString(),
         };
         console.log(comment);
@@ -63,13 +64,13 @@ const NewComment = () => {
         <button
           type="submit"
           id="addCommentBtn"
-          className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+          className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none f"
         >
           Add Comment
         </button>
         <button
           onClick={closeModal}
-          className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 mx-3 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+          className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 mx-3 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none f"
         >
           Cancel
         </button>
